4c1ab22a12b986510efc9d0ebdbc106a7bf6a5e4
Yamil Español Coloma <57334377+yamil-90@users.noreply.github.com>
2022-05-30 22:06:33 -0300

    agregue cosas

--- ej6.py
+++ ej6.py
@@ -1,4 +1,5 @@
 numero = int(input("numero elegido:"))
 suma = int(((numero+1)*numero)/2)
-print("la suma de todos los numeros enteros es", suma)
+print("la suma de todos los numeros enteros desde 1 a " + str(numero) + " es: ", suma)
 #modificacion desde la Web
+#esta es mi modificacion
